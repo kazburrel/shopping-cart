9d2f77747c25c9ee38bddd407f7be89042ed2f70
kazburrel <oegbosionu@gmail.com>
2023-04-17 14:13:28 +0100

    new

---
+++ src/context/ShoppingCartContext.tsx
@@ -0,0 +1,18 @@
+import { ReactNode, createContext, useContext } from "react";
+const ShoppingCartContext = createContext({})
+
+type ShoppingCartProviderProps = {
+    children : ReactNode
+}
+
+export function useShoppingCart(){
+    return useContext(ShoppingCartContext)
+}
+
+export function ShoppingCartProvider({children} : ShoppingCartProviderProps){
+    return (
+        <ShoppingCartContext.Provider value={{}}>
+        {children}
+        </ShoppingCartContext.Provider>
+    )
+}
